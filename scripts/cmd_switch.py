53b58b539c58e326971ccaf2133b24115ad81301
Eric <eric@virtuosorobotics.com>
2019-07-18 16:48:47 +0800

    update switch

--- scripts/cmd_switch.py
+++ scripts/cmd_switch.py
@@ -1,23 +1,29 @@
 #!/usr/bin/env python
 import rospy
+import math
 from std_msgs.msg import *
-from geometry_msgs.msg import Twist
-# from pub_n_sub.msg import Num
+from geometry_msgs.msg import *
+from move_base_msgs.msg import *
 
 pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
 rotation = False
+got_first_goal = False
 
-def two_callback(data):
+goal_pose = Pose
+goal_x = 0
+goal_y = 0
+
+def normal_cmd(data):
     global pub,rotation
     if rotation:
         pub.publish(data)
-        print 'rotation 2'
+        print 'rotation'
 
-def three_callback(data):
+def no_rotation(data):
     global pub,rotation
     if not rotation:
         pub.publish(data)
-        print 'no rotation 3'
+        print 'no rotation'
 
 def rotation_switch(msg):
     global rotation
@@ -27,13 +33,30 @@ def rotation_switch(msg):
     else:
         rotation = False
 
-    
+def update_goal(msg):
+    global goal_x,goal_y,got_first_goal
+    goal_pose = msg.goal.target_pose.pose
+    goal_x = goal_pose.position.x
+    goal_y = goal_pose.position.y
+    got_first_goal = True
+    print goal_pose
 
+def goal_dist(msg):
+    global goal_x,goal_y,rotation
+    if got_first_goal:
+        robot_pose = msg.feedback.base_position.pose
+        dist = math.sqrt((goal_x - robot_pose.position.x)**2+(goal_y - robot_pose.position.y)**2)
+        if dist < 0.3:
+            rotation = True
+        else:
+            rotation = False
+    
 
 if __name__ == '__main__':
     rospy.init_node('rotation_switch', anonymous=False)
-    rospy.Subscriber("cmd_vel_2", Twist, two_callback)
-    rospy.Subscriber("cmd_vel_3", Twist, three_callback)
+    rospy.Subscriber("cmd_vel_normal", Twist, normal_cmd)
+    rospy.Subscriber("cmd_vel_no_rotation", Twist, no_rotation)
     rospy.Subscriber("rotation",Bool,rotation_switch)
+    rospy.Subscriber('/move_base/feedback',MoveBaseActionFeedback,goal_dist)
+    rospy.Subscriber('/move_base/goal',MoveBaseActionGoal,update_goal)
     rospy.spin()
-    # listener()
